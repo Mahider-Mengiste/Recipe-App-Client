ec9892214d1cda7c6178f9abe943c9408aa1113c
mmengi18 <mmengi18@montgomerycollege.edu>
2022-08-25 20:56:02 -0400

    search button is working

--- src/components/recipes/RecipeIndex.js
+++ src/components/recipes/RecipeIndex.js
@@ -30,7 +30,7 @@ const RecipesIndex = (props) => {
     const [error, setError] = useState(false)
 
     const { msgAlert, input, recipes, setRecipes } = props
-    console.log("++++++++++++++=", input,recipes, setRecipes)
+    // console.log("++++++++++++++=", input,recipes, setRecipes)
     
 
     console.log('Props in RecipesIndex', props)
@@ -53,6 +53,7 @@ const RecipesIndex = (props) => {
     async function getMyRecipes () {
         const myRecipes = await getAllRecipes() 
         setRecipes(myRecipes.data.recipes)
+        console.log("this is state recipe", recipes)
         console.log("this is my recipes", myRecipes)
     }
     getMyRecipes()
@@ -166,26 +167,115 @@ const RecipesIndex = (props) => {
             
     ))
         
-        const searchResults = () => {
-        if (input) {
-            const data = recipes.map(recipe => 
-            recipe.recipeName.toLowerCase().includes(input.toLowerCase()))
-                console.log(data)
-                data.map(recipes => (
-                    <h1>data.recipeName</h1>
-                ))
-        }
-    }
-            
-    
-    
-    searchResults()
+    const searchResults = recipes.filter((recipe) => {
+        // recipe.recipeName.toLowerCase().includes(input.toLowerCase()))
+        // .map(recipe => (
+        // <h1> recipe.recipeName </h1>
+        return recipe["recipeName"].includes(input)
         
+    }  
+    )
+        const displayResults = searchResults.map((searchResult) => {
+         return <Card
+                key={searchResult.id}
+                style={
+                    {
+                        backgroundColor: 'rgb(255, 255, 244)',
+                        width: '340px',
+                        height: '350',
+                        borderRadius: '10px',
+                        boxShadow: 'box-shadow: rgba(0, 0, 0, 0.25) 0px 25px 50px -12px',
+                        marginLeft: '10px',
+                        marginRight: '10px',
+                        marginTop: '25px',
+                        marginBottom: '5px',
+                        border: '2px solid 	#D0D0D0'
+                    }
+                }
+                >
+                <Link to={`/recipes/${searchResult._id}`}
+                    style={
+                        {
+                            
+                            textAlign: 'center',
+                            textDecoration: 'none',
+                            fontSize: '19px',
+                            color: 'rgb(56, 55, 55)'
+                        }
+                    }
+                >   
+                    <Card.Header>
+                        {searchResult.recipeName}
+                    </Card.Header>
+                    <Card.Header
+                    style={
+                        {
+                            fontSize: '15px'
+                        }
+                    }
+                    >
+                        Creater Name: {searchResult.recipeCreater}
+                    </Card.Header>
+                </Link>
+                <Card.Body  
+                style={
+                    {
+                        // backgroundColor: 'red',
+                        maxWidth: '17rem',
+                        margin: 'auto'
+                        
+
+                    }
+                }
+                >
+                    <Link to={`/recipes/${searchResult._id}`}>
+                        <img 
+                        src={searchResult.image} 
+                        alt={searchResult.recipeName}
+                        style={
+                                    {
+                                        // borderBlockStyle: 'dashed',
+                                        maxHeight: '17rem',
+                                        maxWidth: '15rem',
+                                        padding: '0px',
+                                        borderRadius: '4px',
+                                        border: '2px solid #C0C0C0',
+                                        
+
+                            
+                                    }
+                                }
+                        ></img>
+                    </Link>
+                </Card.Body>
+                <Card.Footer>
+                    <div 
+                    style={
+                        {
+                            textAlign: 'center',
+                        }
+                    }>
+                        <Link to={`/recipes/${searchResult._id}`}
+                        style= {
+                            {
+                                textDecoration: 'none',
+                                fontSize: '19px',
+                                color: '#cc0052'
+                            }
+                        }
+                        >
+                            View recipe
+                        </Link>
+                    </div>
+                </Card.Footer>
+            </Card >
+        })
+        console.log("+++==========", searchResults)
     return (
         <div 
         style={ cardContainerStyle }
         >
-            { input ? searchResults : recipeCards }
+            { input ? displayResults  : recipeCards }
         </div>
     )
 }
